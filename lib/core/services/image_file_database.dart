dadc52d59ffb3e779b3a91ef9b0ca1208d485a1a
Maher <34738373+maherjaafar@users.noreply.github.com>
2021-05-01 19:01:32 +0100

    Add image file database

---
+++ lib/core/services/image_file_database.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+
+class ImageFileDatabase {
+  Future<String> getImageFilePath() async {
+    Directory appDocumentsDirectory = await getApplicationDocumentsDirectory(); // 1
+    String appDocumentsPath = appDocumentsDirectory.path; // 2
+    String imageFilePath = '$appDocumentsPath/imagefile.jpg'; // 3
+
+    return imageFilePath;
+  }
+
+  Future<void> saveImageFile(String base64String) async {
+    File imageFile = File(await getImageFilePath()); // 1
+    await imageFile.writeAsBytes(Base64Decoder().convert(base64String)); // 2
+  }
+
+  Future<String> readImageFile() async {
+    File imageFile = File(await getImageFilePath()); // 1
+    final imageBytes = await imageFile.readAsBytes();
+    print(imageBytes);
+    String imageFileContent = base64Encode(imageBytes);
+    print('File Content: $imageFileContent');
+
+    return imageFileContent;
+  }
+
+  Future<File> readFile() async {
+    File imageFile = File(await getImageFilePath()); // 1
+    return imageFile;
+  }
+}
